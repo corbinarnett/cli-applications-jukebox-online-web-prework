b7174d53e96e2d16fcd98ff3c0d4ef7fc5ff5dac
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-07-01 21:29:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -69,3 +69,16 @@ def run(songs)
     end
   exit_jukebox
 end
+
+# case x
+# when 1..5
+#   "It's between 1 and 5"
+# when 6
+#   "It's 6"
+# when "foo", "bar"
+#   "It's either foo or bar"
+# when String
+#   "You passed a string"
+# else
+#   "You gave me #{x} -- I have no idea what to do with that."
+# end
